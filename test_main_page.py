6c375781fa31cebdd25be9345aaa973ce8c5a67d
Alisa Sandovskaya <rikkona86@gmail.com>
2024-02-29 11:17:23 +0300

    add some fixes

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,8 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 from pages.locators import MainPageLocators
 from pages.locators import LoginPageLocators
+
 
 def test_guest_can_go_to_login_page(browser):
     link = MainPageLocators.LINK
@@ -15,7 +17,8 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_guest_can_go_to_login_page(browser):
+
+def test_guest_can_go_to_login_form(browser):
     link = LoginPageLocators.LINK
     page = LoginPage(browser, link)
     page.open()
