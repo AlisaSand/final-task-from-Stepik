696971cab8d227f7b8d00896d81ec81cb4d0824d
Alisa Sandovskaya <rikkona86@gmail.com>
2024-02-29 11:13:43 +0300

    delete odd test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from pages.main_page import MainPage
 from pages.locators import MainPageLocators
+from pages.locators import LoginPageLocators
 
 def test_guest_can_go_to_login_page(browser):
     link = MainPageLocators.LINK
@@ -13,4 +14,11 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_can_go_to_login_page(browser):
+    link = LoginPageLocators.LINK
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+
 
